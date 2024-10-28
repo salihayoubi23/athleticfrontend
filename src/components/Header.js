2995f451cc9004e5a6f877bf50f378b910d4303f
salihayoubi23 <salihayoubi23@outlook.com>
2024-10-28 22:05:27 +0100

    header

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,11 +2,12 @@ import React from 'react';
 import { Navbar, Nav, Container } from 'react-bootstrap';
 import { Link, useNavigate } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { useCart } from '../components/CartContext'; 
+import { useCart } from '../components/CartContext';
+
 const Header = () => {
-    const { cartItems } = useCart(); // Utilisez le contexte du panier pour obtenir les articles
+    const { cartItems } = useCart();
     const navigate = useNavigate();
-    const isAuthenticated = !!localStorage.getItem('token'); 
+    const isAuthenticated = !!localStorage.getItem('token');
 
     const handleLogout = () => {
         localStorage.removeItem('token');
@@ -23,26 +24,29 @@ const Header = () => {
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="ms-auto">
                         <Nav.Link as={Link} to="/" className="text-warning">Accueil</Nav.Link>
-                        {isAuthenticated && (
-                            <Nav.Link as={Link} to="/userprofil" className="text-warning">
-                                Mon Profil
-                            </Nav.Link>
-                        )}
-                        <Nav.Link as={Link} to="/cart" className="text-warning">
-                            Mon Panier
-                            {cartItems.length > 0 && (
-                                <span className="badge bg-warning text-dark ms-2">
-                                    {cartItems.length}
-                                </span>
-                            )}
-                        </Nav.Link>
-                        {!isAuthenticated ? (
+
+                        {isAuthenticated ? (
+                            <>
+                                <Nav.Link as={Link} to="/userprofil" className="text-warning">
+                                    Mon Profil
+                                </Nav.Link>
+                                <Nav.Link as={Link} to="/cart" className="text-warning">
+                                    Mon Panier
+                                    {cartItems.length > 0 && (
+                                        <span className="badge bg-warning text-dark ms-2">
+                                            {cartItems.length}
+                                        </span>
+                                    )}
+                                </Nav.Link>
+                                <Nav.Link onClick={handleLogout} className="text-warning">
+                                    Se déconnecter
+                                </Nav.Link>
+                            </>
+                        ) : (
                             <>
                                 <Nav.Link as={Link} to="/login" className="text-warning">Se connecter</Nav.Link>
                                 <Nav.Link as={Link} to="/signup" className="text-warning">Créer un compte</Nav.Link>
                             </>
-                        ) : (
-                            <Nav.Link onClick={handleLogout} className="text-warning">Se déconnecter</Nav.Link>
                         )}
                     </Nav>
                 </Navbar.Collapse>
